ec92113c8bcf7bbe6955f46a91cb685ce6ef46c4
erikavillamin <erikamaevillamin@gmail.com>
2025-05-05 20:16:27 +0800

    process_questions_block_into_structured_data

--- quiz_taker.py
+++ quiz_taker.py
@@ -1,4 +1,8 @@
 file_name = "quiz_creator_questions.txt"
-with open(file_name, 'r') as file:
+with open(file_name, 'r') as file: # open and read file
     questions = file.read().strip()
-    
+    blocks = questions.split('\n\n') # question is separated by a blank line
+
+quiz = [] 
+for block in blocks: # it is a loop over each question block
+    lines = block.strip().split('\n')  #this will split the block into separate lines
