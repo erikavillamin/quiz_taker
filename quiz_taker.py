84fff3cee22abd42bab2676e62cf65c63c959344
erikavillamin <erikamaevillamin@gmail.com>
2025-05-05 21:01:54 +0800

    ensures_answer_line_correctly_formatted

--- quiz_taker.py
+++ quiz_taker.py
@@ -9,3 +9,9 @@ for block in blocks: # it is a loop over each question block
 
     question = lines[0][len("Question: "):] # extract question
     choices = lines[1:5]  # four lines after question
+
+    answer = lines[5]  #ensure the line for answer is correctly formatted 
+    if not answer.lower().startswith('answer: '): # the line should start with answer, skip if not
+        print("Invalid")
+        continue
+    quiz_answer = answer[len('answer: '):].strip().upper  # the answer is stored as a single uppercase letter as correct answer
